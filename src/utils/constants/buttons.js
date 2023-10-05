1eae2e06351a108157283a3917b421c778891b12
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-05 11:13:10 -0300

    add array w/ buttons

---
+++ src/utils/constants/buttons.js
@@ -0,0 +1,92 @@
+export const buttons = [
+  {
+    id: 'clear',
+    value: 'AC',
+    className: 'operator'
+  },
+  {
+    id: 'DEL',
+    value: 'DEL',
+    className: 'operator'
+  },
+  {
+    id: 'divide',
+    value: '/',
+    className: 'operator'
+  },
+  {
+    id: 'seven',
+    value: '7',
+    className: 'number'
+  },
+  {
+    id: 'eight',
+    value: '8',
+    className: 'number'
+  },
+  {
+    id: 'nine',
+    value: '9',
+    className: 'number'
+  },
+  {
+    id: 'multiply',
+    value: '*',
+    className: 'operator'
+  },
+  {
+    id: 'four',
+    value: '5',
+    className: 'number'
+  },
+  {
+    id: 'five',
+    value: '5',
+    className: 'number'
+  },
+  {
+    id: 'six',
+    value: '6',
+    className: 'number'
+  },
+  {
+    id: 'subtract',
+    value: '-',
+    className: 'operator'
+  },
+  {
+    id: 'one',
+    value: '1',
+    className: 'number'
+  },
+  {
+    id: 'two',
+    value: '2',
+    className: 'number'
+  },
+  {
+    id: 'three',
+    value: '3',
+    className: 'number'
+  },
+  {
+    id: 'add',
+    value: '+',
+    className: 'operator'
+  },
+  {
+    id: 'zero',
+    value: '0',
+    className: 'number'
+  },
+  {
+    id: 'decimal',
+    value: '.',
+    className: ''
+  },
+  {
+    id: 'equals',
+    value: '=',
+    className: 'operator'
+  }
+]
