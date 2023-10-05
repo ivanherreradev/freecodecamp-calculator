8a84bc5c57a5b22e24022497739d625edb56e357
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-05 11:31:22 -0300

    add styles (Button)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styles from './Button.module.css'
 
 const Button = ({ id, value, onClick, className }) => {
   return (
-    <button id={id} onClick={() => onClick(value)} className={className}>
+    <button
+      id={id}
+      onClick={() => onClick(value)}
+      className={`${styles.button} ${className}`}
+    >
       {value}
     </button>
   )
