4d71bcab6625f02a67c3101ddc4c05011fa3aaa7
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-05 11:31:10 -0300

    add styles (Display)

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './Display.module.css'
 
 const Display = ({ value }) => {
   return (
-    <header id="display">
+    <header id="display" className={styles.display}>
       {value}
     </header>
   )
